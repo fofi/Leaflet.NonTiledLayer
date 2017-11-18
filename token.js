08abb8f69fde22e65919426aab1611f5d46e1deb
unknown <Marvin@wn-esx-mmbuild.ptvag.ptv.de>
2017-11-18 01:52:34 +0100

    Token update

--- token.js
+++ token.js
@@ -1,3 +1,3 @@
 // The token below is for internal use only - consider getting your own token!
-// This token is valid until 2018-01-12T17:05:34Z and was auto-generated @ 2017-11-16T23:11:49Z.
-var token = 'F1574172-87AF-4319-BE5E-6447878C78B1';
+// This token is valid until 2018-02-09T18:03:21Z and was auto-generated @ 2017-11-17T23:13:08Z.
+var token = '5D213BA1-B65E-4D10-9DB8-37FDD30D1399';
