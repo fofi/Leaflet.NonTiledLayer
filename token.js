e60defdddffd106377ae39710c8652f03d56f98d
unknown <Marvin@wn-esx-mmbuild.ptvag.ptv.de>
2017-11-29 01:21:23 +0100

    Token update

--- token.js
+++ token.js
@@ -1,3 +1,3 @@
 // The token below is for internal use only - consider getting your own token!
-// This token is valid until 2018-02-09T18:03:21Z and was auto-generated @ 2017-11-27T23:54:44Z.
+// This token is valid until 2018-02-09T18:03:21Z and was auto-generated @ 2017-11-28T22:25:45Z.
 var token = '5D213BA1-B65E-4D10-9DB8-37FDD30D1399';
