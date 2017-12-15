a35c1ec593330602aefe2bd0cfbc199bc7a8ba88
unknown <Marvin@wn-esx-mmbuild.ptvag.ptv.de>
2017-12-16 00:53:33 +0100

    Token update

---
+++ token.js
@@ -0,0 +1,3 @@
+// The token below is for internal use only - consider getting your own token!
+// This token is valid until 2018-03-09T18:06:11Z and was auto-generated @ 2017-12-15T22:22:47Z.
+var token = '0B5DE87D-8A43-46BD-8606-81877BAF244F';
