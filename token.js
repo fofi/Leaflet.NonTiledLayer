0c9f9352fdd995c3aa2347a827594cc070c986d7
unknown <Marvin@wn-esx-mmbuild.ptvag.ptv.de>
2017-12-28 01:21:13 +0100

    Token update

--- token.js
+++ token.js
@@ -1,3 +1,3 @@
 // The token below is for internal use only - consider getting your own token!
-// This token is valid until 2018-03-09T18:06:11Z and was auto-generated @ 2017-12-26T22:16:53Z.
+// This token is valid until 2018-03-09T18:06:11Z and was auto-generated @ 2017-12-27T23:10:33Z.
 var token = '0B5DE87D-8A43-46BD-8606-81877BAF244F';
